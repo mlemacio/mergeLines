c69ee3b88facd289512d6ae46b2d0031d1af986c
Mac Mleczko <mlemacio0@gmail.com>
2023-02-05 20:14:45 -0600

    Finish out main.cpp outline

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
-#include "take_home_test/deser.h"
+#include "deser.h"
+#include "line_merger.h"
 
 const std::string INPUT_FILE = "/Users/maciejmleczko/projects/cpp/mergeLines/test/assets/1-split.json";
 const std::string OUTPUT_FILE = "/Users/maciejmleczko/projects/cpp/mergeLines/test/assets/1-output.json";
@@ -7,8 +7,16 @@ const std::string OUTPUT_FILE = "/Users/maciejmleczko/projects/cpp/mergeLines/te
 auto main() -> int
 {
     // Deserialize in from file
-    std::vector<lineSegment_t> lines = deserialize_from_file(INPUT_FILE);
-    serialize_to_file(lines, OUTPUT_FILE);
+    auto lines = deserialize_from_file(INPUT_FILE);
+
+    // Merge the lines
+    lineMerger_t lm;
+    lm.addLineSegments(std::move(lines));
+    lm.processLines();
+    auto merged_lines = lm.getMergedLines();
+
+    // Serialize the merged lines to the output file
+    serialize_to_file(std::move(merged_lines), OUTPUT_FILE);
 
     return 0;
 }
