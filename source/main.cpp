c8192423914f365ddd069db237192f4910603957
Mac Mleczko <mlemacio0@gmail.com>
2023-02-05 19:02:34 -0600

    Basic deser infrastructure now there

--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include "take_home_test/deser.h"
 
-const std::string INPUT_FILE = "../";
+const std::string INPUT_FILE = "/Users/maciejmleczko/projects/cpp/mergeLines/test/assets/1-split.json";
+const std::string OUTPUT_FILE = "/Users/maciejmleczko/projects/cpp/mergeLines/test/assets/1-output.json";
 
 auto main() -> int
 {
     // Deserialize in from file
     std::vector<lineSegment_t> lines = deserialize_from_file(INPUT_FILE);
-    serialize_to_file(lines);
+    serialize_to_file(lines, OUTPUT_FILE);
 
     return 0;
 }
